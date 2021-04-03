fbe4b725e1168197e706e8a801d54f2937c4c1aa
Carlos24122322 <cadu.vieira2276@gmail.com>
2021-04-02 22:02:01 -0300

    Projeto compilando perfeitamente, sistema CRUD de cadastro de series em .NET

---
+++ Entities/Serie.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Text;
+using Dio.Series;
+namespace Dio.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo {get; set;}
+        private string Descricao {get; set;}
+        private int Ano {get; set;}
+        private bool Excluido{get; set;}        
+
+
+        public Serie(int id, Genero genero, string titulo, 
+        string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;    
+            this.Excluido = false;        
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        /* 
+            public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string retorno = "";
+            sb.Append(retorno +="Genero: ");
+            sb.Append(Genero);
+            sb.Append(retorno += "Titulo: ");
+            sb.Append(Titulo);
+            sb.Append(retorno += "Descrição: ");
+            sb.Append(Descricao);
+            sb.Append(retorno += "Ano de início: ");
+            sb.Append(Ano);
+            return retorno.ToString();            
+        }
+        
+        */
+        
+
+        // Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+
+            public override string ToString()
+        {            
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido;
+            return retorno;
+        }
+        
+    }    
+}
+
+
---
+++ Entities/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Dio.Series.Interfaces;
+using Dio.Series;
+namespace Dio.Series
+{
+    
+    public class SerieRepositorio : IRepositorio<Serie> 
+    {
+        private List<Serie> listaSerie = new List<Serie>(); // Variável que é uma lista de séries
+        public void Atualizar(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto; // o id é do parâmetro e fica como indexador
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+        
+    }
+}
